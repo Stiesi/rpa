99667a9873e14e99424f300c94064b8d968a565d
jaguschj <45715268+jaguschj@users.noreply.github.com>
2022-06-02 22:37:13 +0200

    uploading files

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
-from fastapi import FastAPI
+#from typing import List
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
 import numpy as np
+import pandas as pd
+from scipy.optimize import leastsq
 app = FastAPI()
 
 
 def add(a,b):
     return a+b
 
-@app.get("/")
-async def root():    
-    return "Hello World! MannJens"
-
 
 @app.post("/add/")
 async def getadd(a: float, b:float):    
     c=add(a,b)
     return { "c" : c }
+
+@app.post("/files/")
+async def create_files(files: list[bytes]):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
